ce6bef7709693b445946a8b19fbdcb4a79c0c193
guillermo_salazar <guillermos_2007@yahoo.com>
2015-06-10 13:56:32 +0000

    added date time picker

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,36 @@
 class TasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :all_tasks,only: [:index, :create, :update, :destroy]
 
-  # GET /tasks/new
+  # index and show actions removed
+  def destroy
+  @task.destroy
+  end
+  
+  def update
+     @task.update(task_params) 
+  end  
+    
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
-  def edit
-  end
-  
-  # POST /tasks
-  # POST /tasks.json
   def create
-    @task = Task.create(task_params)
-    @task.save
-  end
-  
-  def update
-    @task = Task.update(task_params)
-  end
-  
-  def destroy
-    @task.destroy
+    #@task = Task.new(task_params)
+    @task  = Task.create(task_params)
   end
 
   private
+
+    # new action
     def all_tasks
       @tasks = Task.all
     end
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:description, :deadline)
     end
